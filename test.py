f24079c6defaea460bf7545acd4224181e1f0529
Miya277 <75023450+Miya277@users.noreply.github.com>
2021-10-09 20:13:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
-knn__data = pd.read_csv('pai_data/mydata/knn_house_data.csv',sep='\t',header=0,usecols=name)
+# knn__data = pd.read_csv('pai_data/mydata/knn_house_data.csv',sep='\t',header=0,usecols=name)
+print('Hello')
